41966ba548cb7d0731348e1a2db0db7e682cc40e
Gagan Deep Singh <gagangulyanig@gmail.com>
2019-09-10 00:27:02 +0530

    Added Background Images
    Added Background images,
    modified js file along with css for new backgrounds..
    edited index.html for clearing out test code.

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,8 +1,17 @@
-var imgs = new Array("camera2.jpg","cinema.jpg","drums.jpg","guitar.jpg");
+var imgs = new Array(
+"avel-chuklanov-rWazZf6qDFM-unsplash.jpg",
+"celine-druguet-ttzoSPBYdrI-unsplash.jpg",
+"chris-bair-A10y2Eq7OHY-unsplash.jpg",
+"clem-onojeghuo-xJXxMR5PXoY-unsplash.jpg",
+"david-hurley-hjSt16JGyEE-unsplash.jpg",
+"davisco-rhUU1pemhQ0-unsplash.jpg",
+"jakob-owens-CiUR8zISX60-unsplash.jpg",
+"keagan-henman-pPxJTtxfV1A-unsplash.jpg",
+"allef-vinicius-P2BoE6tb8ig-unsplash.jpg");
 
 function changeBg() {
     var imgUrl = imgs[Math.floor(Math.random()*imgs.length)];
-    $('.site-space-background').css('background-image', 'url("/static/images/' + imgUrl + '")');
+    $('.site-space-background').css('background-image', 'url("/static/images/backgrounds/' + imgUrl + '")');
     $('.site-space-background').fadeIn(1000); //this is new, will fade in smoothly
 }
 
