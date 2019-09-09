a671c64e6cfb3af81174dce90e43c9df309a2c6f
Mayank Setia <mayanksetia13@gmail.com>
2019-09-10 01:32:49 +0530

    yo
    yo

--- forms.py
+++ forms.py
@@ -2,11 +2,18 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
 
-class register(FlaskForm):
+class LoginForm(FlaskForm):
+    
+    email = StringField('Email')
+    password = StringField('Password')
+    submit = SubmitField('Login')
+
+
+class Join(FlaskForm):
     name = StringField('Name')
     city = StringField('City')
     number = StringField('Contact Number')
     email = StringField('Email')
     password = StringField('Password')
     cpassword = StringField('Confirm Password')
-    submit = SubmitField('Register')
+    submit = SubmitField('Register')    
--- login.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
-
-class login(FlaskForm):
-    
-    email = StringField('Email')
-    password = StringField('Password')
-    submit = SubmitField('Login')
