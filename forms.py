9ef8ccfff35cd59525906163f43fb939c1fa6717
Mayank Setia <mayanksetia13@gmail.com>
2019-09-14 18:54:23 +0530

    Form Validators
    wtfforms.Validators Used

--- forms.py
+++ forms.py
@@ -1,19 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-
+from wtforms.validators import DataRequired , Length , Email , EqualTo, ValidationError
 
 class Login(FlaskForm):
     
-    email = StringField('Email')
-    password = StringField('Password')
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = StringField('Password',validators=[DataRequired()])
     submit = SubmitField('Login')
 
 
 class Register(FlaskForm):
-    name = StringField('Name')
-    city = StringField('City')
-    number = StringField('Contact Number')
-    email = StringField('Email')
-    password = StringField('Password')
-    cpassword = StringField('Confirm Password')
+    name = StringField('Name',validators=[DataRequired(),Length(min=2,max=20)])
+    city = StringField('City',validators=[DataRequired()])
+    number = StringField('Contact Number',validators=[DataRequired()])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = StringField('Password',validators=[DataRequired()])
+    cpassword = StringField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
     submit = SubmitField('Register')    
