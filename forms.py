0357d47ae3995cf8cd27c3a7cb4dce04d71de54f
Mayank Setia <mayanksetia13@gmail.com>
2019-09-14 19:06:58 +0530

    validators

--- forms.py
+++ forms.py
@@ -1,19 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired , Length , Email , EqualTo, ValidationError
+from wtforms.validators import InputRequired , Length , Email , EqualTo, ValidationError
 
 class Login(FlaskForm):
     
-    email = StringField('Email',validators=[DataRequired(),Email()])
-    password = StringField('Password',validators=[DataRequired()])
+    email = StringField('Email',validators=[InputRequired(),Email(),Length(min=10,max=25)])
+    password = StringField('Password',validators=[InputRequired()],Length(min=8))
     submit = SubmitField('Login')
 
 
 class Register(FlaskForm):
-    name = StringField('Name',validators=[DataRequired(),Length(min=2,max=20)])
-    city = StringField('City',validators=[DataRequired()])
-    number = StringField('Contact Number',validators=[DataRequired()])
-    email = StringField('Email',validators=[DataRequired(),Email()])
-    password = StringField('Password',validators=[DataRequired()])
-    cpassword = StringField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
+    name = StringField('Name',validators=[InputRequired(),Length(min=2,max=20)])
+    city = StringField('City',validators=[InputRequired()])
+    number = StringField('Contact Number',validators=[InputRequired()])
+    email = StringField('Email',validators=[InputRequired(),Email()])
+    password = StringField('Password',validators=[InputRequired()])
+    cpassword = StringField('Confirm Password',validators=[InputRequired(),EqualTo('password')])
     submit = SubmitField('Register')    
