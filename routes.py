63b314c0247aa9e47edee23463329ec747694acb
RitikBhatnagar <55330188+RitikBhatnagar@users.noreply.github.com>
2019-11-10 13:11:07 +0530

    update: About & Contact Page

--- routes.py
+++ routes.py
@@ -210,6 +210,13 @@ def followers(username=None):
 def explore():
     return render_template('explore.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 @app.route('/logout')
 @login_required
